f499d1df576e1700bf55a261cec3605cb71468a7
Kord Campbell <kordless@gmail.com>
2023-08-28 15:50:36 -0500

    sloth.py installed

---
+++ sloth.py
@@ -0,0 +1,24 @@
+from InstructorEmbedding import INSTRUCTOR
+model = INSTRUCTOR('hkunlp/instructor-large')
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/embed', methods=['POST'])
+def embed():
+    if request.method == 'POST':
+        data = request.json  # Assuming the data is sent as JSON in the request body
+
+        embeddings = model.encode([data.get('sentence')]).tolist()
+
+        # Process the data and generate a response
+        response_data = {
+            "message": "Received POST request at /embed",
+            "embeddings": embeddings
+        }
+
+        return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
