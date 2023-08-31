7cca491556ea6105dbe88fdda6c22beafe08d2c0
Kord Campbell <kordless@gmail.com>
2023-08-31 14:30:51 -0500

    generate secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -1,1 +1,6 @@
-TOKEN=f00bar
+#!/bin/bash
+
+random_md5=$(openssl rand -hex 16)
+echo "TOKEN=$random_md5" > secrets_tmp.sh
+cp secrets_tmp.sh controller/secrets.sh
+mv secrets_tmp.sh sloth/secrets.sh
